bb9de4cda4555e6ae75c9015d2b3c743c5422efd
mayurwagh <mayurwagh1412@gmail.com>
2026-03-20 20:10:37 +0530

    frontend pipeline is added

--- frontend/frontend-pipeline.groovy
+++ frontend/frontend-pipeline.groovy
@@ -1,27 +1,25 @@
 pipeline{
     agent any 
     stages{
-        stage('Code-pull'){
+        stage('code-pull'){
             steps{
-                git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git'
-            }
+                 git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git' 
+            }   
         }
         stage('Build'){
             steps{
-                sh''' 
+                sh '''
                     cd frontend
                     npm install
                     npm run build
                 '''
             }
         }
-        stage('Deploy'){
+        stage('deploy')
             steps{
                 sh '''
-                    cd frontend
                     aws s3 sync dist/ s3://cblkdfsfdsc-front12end-project-bux/
-                '''    
+                '''
             }
-        }
     }
 }
