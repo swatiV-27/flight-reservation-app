b5d3d218831799dc5243a5d297bf9aae8b8a276b
Swati Verma <249419904+swatiV-27@users.noreply.github.com>
2026-04-22 13:18:46 +0530

    Update Backend.groovy

--- FlightReservationApplication/Backend.groovy
+++ FlightReservationApplication/Backend.groovy
@@ -1,21 +1,45 @@
 pipeline {
     agent any
     stages {
-        stage('Code-pull') {
+        stage('Git Pull') {
             steps {
-                git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git'
+                  git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git'
             }
         }
-        stage('Test') {
+        stage('CODE BUILD') {
             steps {
-                echo 'Testing...'
-                // Add your test commands here
+                sh '''
+                   cd FlightReservationApplication
+                   mvn clean package
+                   '''
             }
         }
-        stage('Deploy') {
+        stage('QA-TEST') {
             steps {
-                echo 'Deploying...'
-                // Add your deploy commands here
+                withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-token') {
+                    sh '''
+                    cd FlightReservationApplication
+                    mvn sonar:sonar -Dsonar.projectKey=flight-reservation-backend 
+                 }    
+            }
+        }
+        stage('Docker') {
+            steps {
+                sh '''
+                    cd FlightReservationApplication
+                    docker build -t mayurwagh/flight-reservation-pls-19-20:latest . 
+                    docker push mayurwagh/flight-reservation-pls-19-20:latest 
+                    docker rmi mayurwagh/flight-reservation-pls-19-20:latest
+                    '''
+            }
+        }
+       stage('Deploy') {
+            steps {
+                sh '''
+                     cd FlightReservationApplication
+                     kubectl apply -f k8s/deployment.yaml
+                     kubectl apply -f k8s/service.yaml
+                  '''
             }
         }
     }
