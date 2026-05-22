cb75ba5dbab5f90457451dabf41bbe89b5dae449
Swati Verma <249419904+swatiV-27@users.noreply.github.com>
2026-05-22 16:52:12 +0530

    Update Backend.groovy

--- FlightReservationApplication/Backend.groovy
+++ FlightReservationApplication/Backend.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Git Pull') {
             steps {
-                  git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git'
+                  git branch: 'main', url: 'https://github.com/swatiV-27/flight-reservation-app.git'
             }
         }
         stage('CODE BUILD') {
