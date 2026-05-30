1707d9744caf911d7bf9fa1ad6ffdf0e4eff8433
Swati Verma <249419904+swatiV-27@users.noreply.github.com>
2026-05-30 16:16:14 +0530

    Update Backend.groovy

--- FlightReservationApplication/Backend.groovy
+++ FlightReservationApplication/Backend.groovy
@@ -19,7 +19,8 @@ pipeline {
                 withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-token') {
                     sh '''
                     cd FlightReservationApplication
-                    mvn sonar:sonar -Dsonar.projectKey=flight-reservation-backend 
+                    mvn sonar:sonar -Dsonar.projectKey=flight-reservation-backend
+                    '''
                  }    
             }
         }
